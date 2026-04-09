246bb0f90636d18e72cf8e778038320fc4e13124
Nikita Shabanov <nik.shabanov2018@yandex.com>
2026-04-10 01:45:30 +0300

    refactor: organize public imports

--- src/conformly/__init__.py
+++ src/conformly/__init__.py
@@ -1,4 +1,30 @@
 from ._builtin_parsing_adapters import *  # noqa: F403
+from ._internal.constraints import (
+    GreaterOrEqual,
+    GreaterThan,
+    LessOrEqual,
+    LessThan,
+    MaxLength,
+    MinLength,
+    MultipleOf,
+    OneOf,
+)
+from ._internal.fields import Email, IPv4, IPv6, IPvAny
 from .core import case, cases
 
-__all__ = ["case", "cases"]
+__all__ = [
+    "Email",
+    "GreaterOrEqual",
+    "GreaterThan",
+    "IPv4",
+    "IPv6",
+    "IPvAny",
+    "LessOrEqual",
+    "LessThan",
+    "MaxLength",
+    "MinLength",
+    "MultipleOf",
+    "OneOf",
+    "case",
+    "cases",
+]
