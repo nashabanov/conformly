4829e494d6722198504bbad365a835280ca14491
Nikita Shabanov <nik.shabanov2018@yandex.com>
2026-04-27 03:09:47 +0300

    refactor(parser): extract if/elif/else logic to _build_node function

--- src/conformly/_internal/parser/extractors/types.py
+++ src/conformly/_internal/parser/extractors/types.py
@@ -53,104 +53,7 @@ TypeNode = ScalarNode | ListNode | DictNode
 def normalize_type(field_type: Any, field_name: str) -> TypeNode:
     t, annotated_constraints = _unwrap_annotated(field_type)
     t, nullable = _unwrap_optional(t, field_name)
-    origin = get_origin(t)
-
-    if origin in (list, set, frozenset):
-        args = get_args(t)
-        if not args:
-            raise SchemaError(
-                f"Field '{field_name}': empty collection",
-                context={
-                    "code": "empty_collection",
-                    "field_name": field_name,
-                    "field_type": repr(field_type),
-                },
-            )
-
-        item_node = normalize_type(args[0], field_name)
-
-        intrinsic_constraints = (
-            (UniqueItems(True),) if origin in (set, frozenset) else ()
-        )
-
-        return ListNode(
-            kind="list",
-            runtime_type=origin,
-            origin=origin,
-            nullable=nullable,
-            constraints=(*annotated_constraints, *intrinsic_constraints),
-            item=item_node,
-        )
-
-    if origin is dict:
-        args = get_args(t)
-        if len(args) != 2:
-            raise SchemaError(
-                f"Field '{field_name}': dict must have 2 type args",
-                context={
-                    "code": "wrong_dict_type_args",
-                    "field_name": field_name,
-                    "field_type": repr(field_type),
-                },
-            )
-
-        key_node = normalize_type(args[0], field_name)
-        value_node = normalize_type(args[1], field_name)
-
-        return DictNode(
-            kind="dict",
-            runtime_type=dict,
-            nullable=nullable,
-            constraints=annotated_constraints,
-            key=key_node,
-            value=value_node,
-        )
-
-    if get_origin(t) is Literal:
-        values = get_args(t)
-        if not values:
-            raise SchemaError(
-                f"Field '{field_name}': empty Literal",
-                context={
-                    "code": "empty_literal",
-                    "field_name": field_name,
-                    "field_type": repr(field_type),
-                },
-            )
-
-        return ScalarNode(
-            kind="scalar",
-            runtime_type=ENUMERATED_TYPE,
-            nullable=nullable,
-            constraints=(*annotated_constraints, OneOf(values)),
-        )
-
-    if isinstance(t, type) and issubclass(t, Enum):
-        values = tuple(member.value for member in t)
-
-        return ScalarNode(
-            kind="scalar",
-            runtime_type=ENUMERATED_TYPE,
-            nullable=nullable,
-            constraints=(*annotated_constraints, OneOf(values)),
-        )
-
-    if isinstance(t, type):
-        return ScalarNode(
-            kind="scalar",
-            runtime_type=t,
-            nullable=nullable,
-            constraints=annotated_constraints,
-        )
-
-    raise SchemaError(
-        f"Field '{field_name}': unsupported type",
-        context={
-            "code": "unsupported_type",
-            "field_name": field_name,
-            "field_type": repr(field_type),
-        },
-    )
+    return _build_node(t, field_name, nullable, annotated_constraints)
 
 
 def _unwrap_annotated(t: Any) -> tuple[Any, tuple[Constraint, ...]]:
@@ -179,3 +82,98 @@ def _unwrap_optional(t: Any, field_name: str) -> tuple[Any, bool]:
         )
 
     return t, False
+
+
+def _build_node(
+    t: Any,
+    field_name: str,
+    nullable: bool,
+    constraints: tuple[Constraint, ...],
+) -> TypeNode:
+    origin = get_origin(t)
+    args = get_args(t)
+
+    if origin in (list, set, frozenset):
+        if not args:
+            raise SchemaError(
+                f"Field '{field_name}': '{origin.__name__}' must be parameterized",
+                context={
+                    "code": "empty_collection",
+                    "field_name": field_name,
+                    "field_type": repr(t),
+                },
+            )
+        item = normalize_type(args[0], field_name)
+        intrinsic: tuple[Constraint, ...] = (
+            (UniqueItems(True),) if origin in (set, frozenset) else ()
+        )
+        return ListNode(
+            kind="list",
+            runtime_type=origin,
+            origin=origin,
+            nullable=nullable,
+            constraints=(*constraints, *intrinsic),
+            item=item,
+        )
+
+    if origin is dict:
+        if len(args) != 2:
+            raise SchemaError(
+                f"Field '{field_name}': 'dict' requires exactly 2 type arguments",
+                context={
+                    "code": "wrong_dict_type_args",
+                    "field_name": field_name,
+                    "field_type": repr(t),
+                },
+            )
+        return DictNode(
+            kind="dict",
+            runtime_type=dict,
+            nullable=nullable,
+            constraints=constraints,
+            key=normalize_type(args[0], field_name),
+            value=normalize_type(args[1], field_name),
+        )
+
+    if origin is Literal:
+        if not args:
+            raise SchemaError(
+                f"Field '{field_name}': 'Literal' must contain at least one value",
+                context={
+                    "code": "empty_literal",
+                    "field_name": field_name,
+                    "field_type": repr(t),
+                },
+            )
+        return ScalarNode(
+            kind="scalar",
+            runtime_type=ENUMERATED_TYPE,
+            nullable=nullable,
+            constraints=(*constraints, OneOf(args)),
+        )
+
+    if isinstance(t, type) and issubclass(t, Enum):
+        values = tuple(member.value for member in t)
+        return ScalarNode(
+            kind="scalar",
+            runtime_type=ENUMERATED_TYPE,
+            nullable=nullable,
+            constraints=(*constraints, OneOf(values)),
+        )
+
+    if isinstance(t, type):
+        return ScalarNode(
+            kind="scalar",
+            runtime_type=t,
+            nullable=nullable,
+            constraints=constraints,
+        )
+
+    raise SchemaError(
+        f"Field '{field_name}': unsupported type annotation '{t!r}'",
+        context={
+            "code": "unsupported_type",
+            "field_name": field_name,
+            "field_type": repr(t),
+        },
+    )
